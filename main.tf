933dc5a15fb74472354e412903addf8876068453
Edward Montes Hernández <edward.montesh@gmail.com>
2021-04-30 23:04:23 -0500

    ADD ECS Module

--- main.tf
+++ main.tf
@@ -22,3 +22,23 @@ module "vpc" {
 
 }
 
+module "ecs" {
+  source = "terraform-aws-modules/ecs/aws"
+  version = "3.0.0"
+  
+  name = "my-ecs"
+
+  container_insights = true
+
+  capacity_providers = ["FARGATE", "FARGATE_SPOT"]
+
+  default_capacity_provider_strategy = [
+    {
+      capacity_provider = "FARGATE_SPOT"
+    }
+  ]
+
+  tags = {
+    Environment = "Development"
+  }
+}
